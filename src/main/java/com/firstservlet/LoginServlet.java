bbb0362bcc8516d0ca9f4509a066b7d792de86d4
Falgun Patel <falgunmpatel9123@gmail.com>
2025-02-28 23:39:31 +0530

    uc3 - Extend the Servlet to accept a valid Name
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -1,5 +1,10 @@
 package com.firstservlet;
 
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebInitParam;
@@ -7,38 +12,38 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(
-        description = "Login Servlet Testing",
-        urlPatterns = { "/LoginServlet" },
-        initParams = {
-            @WebInitParam(name = "user", value = "Falgun"),
-            @WebInitParam(name = "password", value = "Falgun@123")
-        }
+  description = "Login Servlet Testing",
+  urlPatterns = { "/LoginServlet" },
+  initParams = {
+    //USERNAME MUST START WITH UPPERCASE AND LENGTH SHOULD BE MINIMUM 4
+    @WebInitParam(name = "user", value = "Falgun"),
+    @WebInitParam(name = "password", value = "Falgun@123")
+  }
 )
 
 public class LoginServlet extends HttpServlet{
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String user = req.getParameter("user");
-        String pwd = req.getParameter("pwd");
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String nameRegex="^[A-Z]{1}[a-z]{3,}$";
+    String user = req.getParameter("user");
+    String pwd = req.getParameter("pwd");
 
 
-        String userID = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)) {
-            req.setAttribute("user",user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
-        } else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out  = resp.getWriter();
-            out.println("<font color = red> Either username or password is wrong</font>");
-            rd.include(req, resp);
-        }
-
+    String userID = getServletConfig().getInitParameter("user");
+    String password = getServletConfig().getInitParameter("password");
+    if((userID.equals(user) && password.equals(pwd))&&Pattern.matches(nameRegex,userID)) {
+      req.setAttribute("user",user);
+      req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+    } else {
+      RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+      PrintWriter out  = resp.getWriter();
+      out.println("<font color = red> Either username or password is wrong</font>");
+      rd.include(req, resp);
     }
+
+  }
 
 }
